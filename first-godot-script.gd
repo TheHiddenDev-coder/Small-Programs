7ce24d1559cb2158b593224dc385a8d3ab1c208e
TheHiddenDev <ericboboiciov@gmail.com>
2026-05-10 12:22:37 +0300

    My first ever functional Godot Script

---
+++ first-godot-script.gd
@@ -0,0 +1,13 @@
+extends Node
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$Label.text = "Hello, world"
+	$Label.modulate = Color.GREEN
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("my_action"):
+		$Label.modulate = Color.RED
+	if event.is_action_released("my_action"):
+		$Label.modulate = Color.GREEN
