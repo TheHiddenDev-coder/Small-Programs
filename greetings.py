dba29169239339f92bf4503dc24e623f72815c7b
TheHiddenDev <ericboboiciov@gmail.com>
2026-05-10 19:59:49 +0300

    A small nod to mum

---
+++ greetings.py
@@ -0,0 +1,12 @@
+def friendly_greet(name):
+    print(f'Hi, {name}')
+    print('Welcome on board')
+
+
+def formal_greet(first_name, last_name):
+    print(f'Hello, {first_name} {last_name}')
+    print('Welcome aboard')
+
+
+friendly_greet('Deea')
+formal_greet('Andreea', 'Boboiciov')
