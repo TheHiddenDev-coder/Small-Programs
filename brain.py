1214917ab68ffdec2ff29633e513cdeff8b0444e
TheHiddenDev <ericboboiciov@gmail.com>
2026-05-10 12:46:01 +0300

    Avg. nerd's brain

---
+++ brain.py
@@ -0,0 +1,13 @@
+def brain(input):
+    if input == "social_interaction":
+        print("Unexpected human being")
+    elif input == "deadline":
+        print("Initiate procrastination")
+    elif input == "pizza_order":
+        print("Hell yeah, I'm getting fueled")
+    else:
+        print("Hello there, general Grievous")
+
+
+brain("Anything")
+brain("pizza_order")
